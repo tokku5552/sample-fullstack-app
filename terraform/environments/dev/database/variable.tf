e5c3b88b4cfd210778618d833b09e26f65bc8d47
徳田 真之介 <tokuda_shinnosuke@cyberagent.co.jp>
2024-06-28 21:21:48 +0900

    feat: add terraform base

---
+++ terraform/environments/dev/database/variable.tf
@@ -0,0 +1,12 @@
+variable "account_id" {
+  default = "851725193504"
+}
+variable "region" {
+  default = "ap-northeast-1"
+}
+variable "environment" {
+  default = "dev"
+}
+variable "project" {
+  default = "atom"
+}
---
+++ terraform/modules/database/variable.tf
@@ -0,0 +1,4 @@
+variable "account_id" {}
+variable "region" {}
+variable "environment" {}
+variable "project" {}
